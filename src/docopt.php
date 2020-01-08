b7cc39f274e175794b2307ef22e1d57d159ee86c
rualt <rualnar@gmail.com>
2020-01-08 21:33:16 +0400

    create docopt.php file

---
+++ src/docopt.php
@@ -0,0 +1,15 @@
+<?php
+
+$doc = <<<DOC
+gendiff -h
+
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff (-v|--version)
+
+Options:
+  -h --help                     Show this screen
+  -v --version                  Show version
+DOC;
