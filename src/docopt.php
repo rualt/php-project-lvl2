3bdef470886ee32d1eb91c0f2366331770827c5f
rualt <rualnar@gmail.com>
2020-01-08 22:30:15 +0400

    add option --format

--- src/docopt.php
+++ src/docopt.php
@@ -6,8 +6,10 @@ Generate diff
 Usage:
   gendiff (-h|--help)
   gendiff (-v|--version)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
 
 Options:
   -h --help                     Show this screen
   -v --version                  Show version
+  --format <fmt>                Report format [default: pretty]
 DOC;
