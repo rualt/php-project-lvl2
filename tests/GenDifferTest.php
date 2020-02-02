f628ba8f548051b122e71665e1a3c25cc151eb62
rualt <rualnar@gmail.com>
2020-02-02 19:41:26 +0400

    add test of decoding functuon

--- tests/GenDifferTest.php
+++ tests/GenDifferTest.php
@@ -6,12 +6,20 @@ use PHPUnit\Framework\TestCase;
 
 class GenDifferTest extends TestCase
 {
+    private $file1 = __DIR__ . '/fixtures/data/before.json';
+    private $file2 = __DIR__ . '/fixtures/data/after.json';
+    
+    public function testIfDecoded()
+    {
+        $this->assertIsArray(\Differ\getDecodedData($this->file1), 'in not an array');
+        $this->assertIsArray(\Differ\getDecodedData($this->file2), 'in not an array');
+    }
+
+
     public function testGenDiffer()
     {
-        $file1 = __DIR__ . '/fixtures/data/before.json';
-        $file2 = __DIR__ . '/fixtures/data/after.json';
         require(__DIR__ . '/fixtures/expected.php');
-        $actual = \Differ\genDiffer($file1, $file2);
+        $actual = \Differ\genDiffer($this->file1, $this->file2);
         $this->assertEquals($expected, $actual);
     }
 }
