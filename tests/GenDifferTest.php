a2e4d136c800727e4a01efcffab272f2a0a20e97
rualt <rualnar@gmail.com>
2020-02-03 21:14:46 +0400

    add test for parser

--- tests/GenDifferTest.php
+++ tests/GenDifferTest.php
@@ -6,20 +6,20 @@ use PHPUnit\Framework\TestCase;
 
 class GenDifferTest extends TestCase
 {
-    private $file1 = __DIR__ . '/fixtures/data/before.json';
-    private $file2 = __DIR__ . '/fixtures/data/after.json';
+    private $fileOneJson = __DIR__ . '/fixtures/data/before.json';
+    private $fileTwoJson = __DIR__ . '/fixtures/data/after.json';
+    private $fileOneYaml = __DIR__ . '/fixtures/data/before.yaml';
     
     public function testIfDecoded()
     {
-        $this->assertIsArray(\Differ\getDecodedData($this->file1), 'in not an array');
-        $this->assertIsArray(\Differ\getDecodedData($this->file2), 'in not an array');
+        $this->assertIsArray(\Differ\Parsers\getDecodedData($this->fileOneJson), 'in not an array');
+        $this->assertIsArray(\Differ\Parsers\getDecodedData($this->fileOneYaml), 'in not an array');
     }
-
 
     public function testGenDiffer()
     {
         require(__DIR__ . '/fixtures/expected.php');
-        $actual = \Differ\genDiffer($this->file1, $this->file2);
+        $actual = \Differ\genDiffer($this->fileOneJson, $this->fileTwoJson);
         $this->assertEquals($expected, $actual);
     }
 }
