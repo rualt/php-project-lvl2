db573735f48802e6cc368e3886934d5fa5543ba7
rualt <rualnar@gmail.com>
2020-02-03 21:16:00 +0400

    add dynamic dispatching  data parser

--- src/parsers.php
+++ src/parsers.php
@@ -4,33 +4,18 @@ namespace Differ\Parsers;
 
 use Symfony\Component\Yaml\Yaml;
 
-$autoloadPath1 = __DIR__ . '/../../../autoload.php';
-$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
-if (file_exists($autoloadPath1)) {
-    require_once $autoloadPath1;
-} else {
-    require_once $autoloadPath2;
-}
-
-/*function getDecodedData($filePath, $format)
+function getDecodedData($filePath)
 {
-    return json_decode(file_get_contents($filePath), true);
-} */
+    $format = pathinfo($filePath)['extension'];
 
-$before = [
-    "host" => "hexlet.io",
-    "timeout" => 50,
-    "proxy"  => "123.234.53.22"
-];
+    $mapping = [
+        'yaml' => function ($filePath) {
+            return Yaml::parseFile($filePath);
+        },
+        'json' => function ($filePath) {
+            return json_decode(file_get_contents($filePath), true);
+        }
+    ];
 
-$after = [
-    "timeout" => 20,
-    "verbose" => true,
-    "host" => "hexlet.io"
-];
-
-$yamlBefore = Yaml::dump($before);
-$yamlAfter = Yaml::dump($after);
-
-file_put_contents(__DIR__ . '/../tests/fixtures/before.yaml', $yamlBefore);
-file_put_contents(__DIR__ . '/../tests/fixtures/after.yaml', $yamlAfter);
+    return $mapping[$format]($filePath);
+}
