f4bdd27913423f6c036f168656334b3e08798979
rualt <rualnar@gmail.com>
2020-02-04 21:48:37 +0400

    update test

--- tests/GenDifferTest.php
+++ tests/GenDifferTest.php
@@ -6,20 +6,29 @@ use PHPUnit\Framework\TestCase;
 
 class GenDifferTest extends TestCase
 {
-    private $fileOneJson = __DIR__ . '/fixtures/data/before.json';
-    private $fileTwoJson = __DIR__ . '/fixtures/data/after.json';
-    private $fileOneYaml = __DIR__ . '/fixtures/data/before.yaml';
+    private $fileJson1 = __DIR__ . '/fixtures/data/before.json';
+    private $fileJson2 = __DIR__ . '/fixtures/data/after.json';
+    private $fileYaml1 = __DIR__ . '/fixtures/data/before.yaml';
+    private $fileYaml2 = __DIR__ . '/fixtures/data/before.yaml';
+
     
     public function testIfDecoded()
     {
-        $this->assertIsArray(\Differ\Parsers\getDecodedData($this->fileOneJson), 'in not an array');
-        $this->assertIsArray(\Differ\Parsers\getDecodedData($this->fileOneYaml), 'in not an array');
+        $this->assertIsArray(\Differ\Parsers\getDecodedData($this->fileJson1), 'in not an array');
+        $this->assertIsArray(\Differ\Parsers\getDecodedData($this->fileYaml1), 'in not an array');
     }
 
     public function testGenDiffer()
     {
         require(__DIR__ . '/fixtures/expected.php');
-        $actual = \Differ\genDiffer($this->fileOneJson, $this->fileTwoJson);
+        
+        $actual = \Differ\genDiffer($this->fileJson1, $this->fileJson2);
+        $this->assertEquals($expected, $actual);
+
+        $actual = \Differ\genDiffer($this->fileYaml1, $this->fileYaml2);
+        $this->assertEquals($expected, $actual);
+
+        $actual = \Differ\genDiffer($this->fileJson1, $this->fileYaml2);
         $this->assertEquals($expected, $actual);
     }
 }
